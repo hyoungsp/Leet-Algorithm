7c2277d79465d4aed951a31eff24c88c834795f5
hyoungsun <hyoungsun@uchicago.edu>
2018-08-12 11:12:11 +0900

    modify

--- BinaryTreeLevelTraversal.py
+++ BinaryTreeLevelTraversal.py
@@ -1,12 +1,10 @@
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
-class Solution:
     def levelOrder(self, root):
         """
         :type root: TreeNode
@@ -22,3 +20,23 @@ class Solution:
                 tmp.extend([node.left, node.right])
             level = [node for node in tmp if node] # level = tmp nope! as None does not have 'val'
         return result
+    
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+
+t = TreeNode(3)
+t1 = TreeNode(9)
+t2 = TreeNode(20)
+t.left = t1
+t.right = t2
+t3 = TreeNode(15)
+t4 = TreeNode(7)
+t2.left = t3
+t2.right = t4
+
+print(t.levelOrder(t))
+
+#[[3], [9, 20], [15, 7]]
